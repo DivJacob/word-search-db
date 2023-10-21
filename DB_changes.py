4a487afd5409f84e4c8d40304a99ce49da6587ec
Divya Jacob <>
2023-10-20 18:27:57 -0700

    Use KD tree

--- DB_changes.py
+++ DB_changes.py
@@ -1,4 +1,5 @@
-import pyodbc 
+from scipy.spatial import KDTree
+import numpy as np
 
 with open('FreqWords.csv', 'r') as file:
     # Create an empty list to store the lines
@@ -9,8 +10,8 @@ with open('FreqWords.csv', 'r') as file:
         line = line.strip()
         lines.append(line.lower())
 
-
-dict = {}
+words = []
+vectors = []
 for word in lines:
     arr = [0] * 26
     if word =='FreqWords':
@@ -20,33 +21,27 @@ for word in lines:
             continue
         indx = ord(letter)-ord('a')
         arr[indx] += 1
-    dict[word] = arr
-
-DRIVER_NAME = 'SQL SERVER'
-SERVER_NAME = 'DESKTOP-3K0CDKQ'
-DATABASE_NAME = 'SpatialDatabase'
-
-connection_string = f"""
-        DRIVER={{{DRIVER_NAME}}};
-        SERVER={SERVER_NAME};
-        DATABASE={DATABASE_NAME};
-        Trust_Connection = yes;
-
-        """
-
-conn = pyodbc.connect(connection_string) 
-cursor = conn.cursor()
+    words.append(word)
+    vectors.append(arr)
 
 
-for key, values in dict.items():
-    values_str = "'POINT (" + ' '.join(map(str, values)) + ")'" 
-    key_str = f"'{key}'"
-    insert_sql = f"INSERT INTO SpatialPoints VALUES (geometry::STPointFromText({values_str}, 4326), {key_str})"
-    print(insert_sql)
-    cursor.execute(insert_sql)
+vector_array = np.array(vectors)
 
-conn.commit()
+tree = KDTree(vector_array)
 
-# Close the cursor and connection
-cursor.close()
-conn.close()
+print("Enter query word:")
+queryWord = input()
+wordArray = [0] * 26
+for letter in queryWord:
+    if not letter.isalpha():
+        continue
+    indx = ord(letter)-ord('a')
+    wordArray[indx] += 1
+
+distances, indices = tree.query(wordArray, k=10)
+
+result = []
+for index in indices:
+    result.append(words[index])
+
+print(result)
