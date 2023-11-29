26bf2496485335d101531262109ab8db9d96fb1b
Divya Jacob <>
2023-11-28 21:20:26 -0800

    Merge count and tf-idf vectorization

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from WordSearchKDTree import WordSearchKDTree
 from WordSearchRTree import WordSearchRTree
 from sklearn.decomposition import PCA
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 TOTAL_NEIGHBORS = 10
 PCA_DIMENSION = 15
+VECTOR_TYPE = 1
+ENABLE_PCA = False
+ENABLE_RTREE = False
 
-def generate_vector(word):
+def generate_vector(word, type):
     arr = [0] * 26
-    for letter in word:
-        if letter not in "qazxswedcvfrtgbnhyujmikolp":
-            continue
-        indx = ord(letter)-ord('a')
-        arr[indx] += 1
+    if type == 1: # Word Frequency Vector
+        for letter in word:
+            if letter not in "qazxswedcvfrtgbnhyujmikolp":
+                continue
+            indx = ord(letter)-ord('a')
+            arr[indx] += 1
+    else: # Ascii Vector
+        indx = 0
+        for letter in word:
+            if letter not in "qazxswedcvfrtgbnhyujmikolp":
+                continue
+            arr[indx] = ord(letter)-ord('a')
+            indx += 1
+            if indx == 26:
+                break
     return arr
     
-with open("allwords_dataset.csv", 'r') as file:
+with open("words_alpha.txt", 'r') as file:
     # Create an empty list to store the lines
     lines = []
 
@@ -22,35 +37,70 @@ with open("allwords_dataset.csv", 'r') as file:
     for line in file:
         
         line = line.strip()
-        line=line.lower()
-        line=line.split(',')
-        lines.append(line[1].lower())
+        #line=line.lower()
+        #line=line.split(',')
+        lines.append(line.lower())
 
 
-words = []
+words = lines
 vectors = []
-for word in lines:
-    vectors.append(generate_vector(word))
-    words.append(word)
+CV = CountVectorizer(max_features=26,analyzer="char_wb",lowercase=False,ngram_range=(1,26))
+tfidf = TfidfVectorizer(max_features=26,analyzer="char_wb",lowercase=False,ngram_range=(1,26))
+
+if VECTOR_TYPE == 1 or VECTOR_TYPE == 2:
+    for word in words:
+        vectors.append(generate_vector(word, VECTOR_TYPE))
+elif VECTOR_TYPE == 3:
+    print("Count Vectorizer Reduction :")
+    vectors = CV.fit_transform(words).toarray()
+else:
+    print("Tfidf Vectorizer Reduction :")
+    vectors = tfidf.fit_transform(words).toarray()
 
 ktree = WordSearchKDTree(words, vectors)
-rtree = WordSearchRTree(words, vectors)
+if ENABLE_RTREE:
+    rtree = WordSearchRTree(words, vectors)
 
 print("Enter query word:")
 queryWord = input()
-wordArray = generate_vector(queryWord)
+if VECTOR_TYPE == 1 or VECTOR_TYPE == 2:
+    wordArray = generate_vector(queryWord,VECTOR_TYPE)
+elif VECTOR_TYPE == 3:
+    rwordArray=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 0]
+    wordArray = CV.transform(list(queryWord)).toarray()
+    for i in wordArray:
+        for j in range(0,26):
+            if(j==0):
+                rwordArray[j]=i[j]
+            if(i[j] != 0 and j != 0):
+                rwordArray[j]+=i[j]
+    wordArray = rwordArray
+else:
+    rwordArray=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 0]
+    wordArray = tfidf.transform(list(queryWord)).toarray()
+    for i in wordArray:
+        for j in range(0,26):
+            if(j==0):
+                rwordArray[j]=i[j]
+            if(i[j] != 0 and j != 0):
+                rwordArray[j]+=i[j]
+    wordArray = rwordArray
+
 
 ktree.query(wordArray, TOTAL_NEIGHBORS)
+if ENABLE_RTREE:
+    rtree.query(wordArray, TOTAL_NEIGHBORS)
 
-rtree.query(wordArray, TOTAL_NEIGHBORS)
+if ENABLE_PCA:
+    pca = PCA(n_components=PCA_DIMENSION)  
 
-pca = PCA(n_components=PCA_DIMENSION)  
+    reduced_vectors = pca.fit_transform(vectors)
+    reducedktree = WordSearchKDTree(words, reduced_vectors)
+    if ENABLE_RTREE:
+        reducedrtree = WordSearchRTree(words, reduced_vectors)
+    print("Reduce Dimension")
+    reducedWordArray = pca.transform([wordArray])[0]
 
-reduced_vectors = pca.fit_transform(vectors)
-reducedktree = WordSearchKDTree(words, reduced_vectors)
-reducedrtree = WordSearchRTree(words, reduced_vectors)
-print("Reduce Dimension")
-reducedWordArray = pca.transform([wordArray])[0]
-
-reducedktree.query(reducedWordArray, TOTAL_NEIGHBORS)
-reducedrtree.query(reducedWordArray, TOTAL_NEIGHBORS)
+    reducedktree.query(reducedWordArray, TOTAL_NEIGHBORS)
+    if ENABLE_RTREE:
+        reducedrtree.query(reducedWordArray, TOTAL_NEIGHBORS)
